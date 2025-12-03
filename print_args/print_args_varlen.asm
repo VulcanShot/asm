ba4420a7f5ec8e644ac5fe16a9307dee630a808c
Piero Alvarez <pieromarcelohp@gmail.com>
2025-12-03 08:53:14 +0000

    Reduced number of syscalls

--- print_args/print_args_varlen.asm
+++ print_args/print_args_varlen.asm
@@ -25,39 +25,42 @@ _start:
     xor r12, r12
     xor r13, r13
 
+    mov rbx, [rsp] ; argc = *RSP
+
     ; Handle is_program_name_included
     mov rax, is_program_name_included ; cmp expects source operand to be register
     cmp rax, 0x0
     jnz loop_start ; Dont offset r13 if we want to print the program name
-    cmp QWORD [rsp], 0x1 ; argc = 1 when there are no arguments
-    jz exit ; No arguments to print, so exit
     inc r12
     mov r13, 0x8
 loop_start:
+    cmp r12, rbx
+    jz exit
+
     ; Compute address of argument
     ; Safe registers to use: rbx, r12, r13, r14, r15
     add r13, 0x8
     mov rsi, [rsp + r13] ; Set rsi to the value in the address inside [ ]
                          ; In this case, the value is the pointer to the string
-
-    ; Two Methods:
-    ; 1. Count length of array and print the whole thing
-    ; 2. Print char by char until NUL
-    ; Gonna try second cause its easier, but probably less efficient
+    mov r14, rsi ; used for length = rsi - r14
+    
 next_char:
-    cmp BYTE [rsi], 0x0 ; test is the AND comparison
-    jz next_arg ; If rsi is NUL, jump to end
-  
-    ; Print character
-    mov rax, sys_write
-    mov rdi, sys_stdout
-    mov rdx, 1
-    syscall
-
-    add rsi, 0x1
+    cmp BYTE [rsi], 0x0 ; BYTE because we are reading a single character.
+    jz print_arg ; If rsi is NUL, jump to end
+    add rsi, 0x1 ; Move to next byte
     jmp next_char
 
-next_arg:
+print_arg:
+    ; Compute length
+    sub rsi, r14
+
+    ; Print argument
+    mov rax, sys_write
+    mov rdi, sys_stdout
+    mov rdx, rsi
+    mov rsi, r14
+    syscall
+
     ; Print newline
     mov rax, sys_write
     mov rdi, sys_stdout
@@ -65,10 +68,9 @@ next_arg:
     mov rdx, 1
     syscall
 
-    ; Check loop condition
+    ; Loop
     inc r12
-    cmp r12, [rsp] ; argc = *RSP
-    jnz loop_start
+    jmp loop_start
 exit:
     mov rax, sys_exit
     mov rdi, 0
